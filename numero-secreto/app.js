7ee4736705a035cffe80f97cb6f4ce15af8544b1
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-07-23 20:05:01 -0300

    T001: feat - Implemented game logic for number guessing with user prompts and feedback

--- numero-secreto/app.js
+++ numero-secreto/app.js
@@ -1,5 +1,27 @@
 alert("Boas vindas ao jogo do número secreto!");
 
-let numeroSecreto = 29;
+let numeroSecreto = 5;
+let tentativa;
+let tentativas = 1;
+let palavraTentativas = tentativas > 1 ? "tentativas" : "tentativa";
 
-prompt("Escolha um número entre 1 e 30");
+console.log(numeroSecreto);
+
+while (tentativa != numeroSecreto) {
+  tentativa = prompt("Escolha um número entre 1 e 10");
+
+  if (tentativa == numeroSecreto) {
+    break;
+  } else {
+    if (numeroSecreto > tentativa) {
+      alert(`O número secreto é maior que ${tentativa}`);
+    } else {
+      alert(`O número secreto é menor que ${tentativa}`);
+    }
+    tentativas++;
+  }
+}
+
+alert(
+  `Você acertou! Você descobriu o número secreto (${numeroSecreto}) com ${tentativas} ${palavraTentativas}. Parabéns!`
+);
