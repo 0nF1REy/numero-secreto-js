5f0f5b5818e76b545f45b2734b3d4b360ffed5c1
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-07-25 13:24:29 -0300

    T004: feat - Updated button function name and implement game logic for number guessing

--- 02-numero-secreto/scripts/app.js
+++ 02-numero-secreto/scripts/app.js
@@ -1,3 +1,5 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
 function exibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
   campo.innerHTML = texto;
@@ -6,6 +8,21 @@ function exibirTextoNaTela(tag, texto) {
 exibirTextoNaTela("h1", "Jogo do Número Secreto");
 exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
 
-function verificarChute() {
-  console.log("O botão foi clicado");
+function verificarTentativa() {
+  let tentativa = document.querySelector("input").value;
+
+  if (tentativa == numeroSecreto) {
+    exibirTextoNaTela("h1", "Acertou!");
+    exibirTextoNaTela("p", "Você descobriu o número secreto!");
+  } else {
+    if (tentativa > numeroSecreto) {
+      exibirTextoNaTela("p", "O número secreto é menor!");
+    } else {
+      exibirTextoNaTela("p", "O número secreto é maior!");
+    }
+  }
+}
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1);
 }
